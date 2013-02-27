955c7c360a990accea8d83242a838e0ea256c7e9
JDulin <dulinjd@gmail.com>
2013-02-27 10:34:26 -0500

    Problem 4

--- hw5/hw5.py
+++ hw5/hw5.py
@@ -10,7 +10,7 @@
 # All equations assume differential equations of the form f(y, t, *args)
 #
 
-def f(y, t):
+def f2(y, t):
 	""" Solves the system of Problem 2, defined in (ii). """
 	b = 0.002
 	g = -9.8
@@ -20,6 +20,9 @@ def f(y, t):
 	dydt = v
 	dvdt = -b * v * abs(v)/m
 	return [dydt, dvdt]
+
+def f4(y, t):
+
 
 def rk2(f, y0, t, args = ()):
 	""" Second-Order Runge-Kutta Method to solve the differential equation f.
@@ -55,15 +58,23 @@ def rk4(f, y0, t, args = ()):
 def two():
 	initial   = [0., 8., 9.8]
 	times     = np.linspace(0, 5., 1000)
-	solutions = scipy.integrate.odeint(f, initial, times)
+	solutions = scipy.integrate.odeint(f2, initial, times)
 	position  = solutions[:,0]
 	velocity  = solutions[:,1]
 	height    = np.amax(position)
 
 	print("The highest point the projectile reaches is {0} at time {1}".format(height, times[np.where(position==height)]))
 
+def four():
+	initial  = [1/3]
+	times    = np.arange(0., 1., 0.005)
+	soln2    = rk2(f4, initial, times)
+	soln4    = rk4(f4, initial, times)
+	print("The first solution is {0}".format(soln2[39]))
+	print("The second solutions is {0}".format(soln4[39]))
+
 if __name__ == "__main__":
 	import numpy as np
 	import scipy.integrate
-	two()
+	four()
 
