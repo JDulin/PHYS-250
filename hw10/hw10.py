18ce146c1b0df1b995fd6dc927b5b5a2ce8878d0
JDulin <dulinjd@gmail.com>
2013-04-19 17:04:22 -0400

    Updated HW10

--- hw10/hw10.py
+++ hw10/hw10.py
@@ -16,15 +16,15 @@ def linear_leastsq(x, y, sigma = None):
     else:
         sigma = y.copy.fill(sigma)
 
-    N     = length(x)
+    N     = len(x)
 
     Sx    = sum(x)
     Sy    = sum(y)
     Sxy   = sum(x*y)
     Sxx   = sum(x**2)
     Delta = N * Sxx - Sx**2
-    a1    = (Sxx * Sy - Sx * Sxy) / Delta
-    a2    = (N * Sxy - Sx * Sy) / Delta
+    a1    = (Sxx * Sy - Sx * Sxy) / Delta # intercept
+    a2    = (N * Sxy - Sx * Sy) / Delta # slope
 
     return [a1, a2]
 
@@ -37,7 +37,7 @@ def chisq(model, x, y, sigma = None):
     else:
         sigma = y.copy.fill(sigma)
 
-    return sum(((y - model(x, *params) / sigma )**2)
+    return sum(((y - model(x, *params) / sigma )**2))
 
 def two():
     x = np.array([-2,-1,0,1,2])
@@ -45,14 +45,17 @@ def two():
 
     # Line fit, a1 and a2
     params = linear_leastsq(x, y)
-    print("Parameter a1 is {0}".format(params[0]))
-    print("Parameter a2 is {0}".format(params[0]))
+    a1 = params[0]
+    a2 = params[1]
+    sigma = 1
+    print("Parameter a1 is {0}".format(a1))
+    print("Parameter a2 is {0}".format(a2))
 
-    dof       = length(y) - 2   # 'params' from curve fit
-    chisq     = sum(((y - model(x, *params) / sigma)**2)
-    chisq_red = chisq / dof
+    dof       = len(y) - 2   # 'params' from curve fit
+    chisq     = sum(((y - (a1 + a2 * x)) / sigma)**2)
+    red_chisq = chisq / dof
     print("Chi-squared: {0}".format(chisq))
-    print("Reduced Chi-squared: {0}".format(chisq_red))
+    print("Reduced Chi-squared: {0}".format(red_chisq))
 
 
 def three():
@@ -61,7 +64,7 @@ def three():
 
     # Shift the bins to the center 
     shift = (8/51)
-    histogram(gauss, bins=51, range=(-4.0,4.0))j
+    plot.histogram(gauss, bins=51, range=(-4.0,4.0))
 
     # Curve fit to Gaussian
     params, covar = opt.curve_fit(Gaussian, x, gauss)
@@ -70,8 +73,7 @@ def three():
 
 if __name__ == "__main__":
     import numpy as np
+    import matplotlib as plot
     import scipy.stats as stats
     import scipy.special as spec
-
-
-
+    three()
